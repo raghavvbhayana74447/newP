af3a76481ee740ddde050db92835174a0989a843
raghavvbhayana74447 <raghavbhayana.kainskep@gmail.com>
2025-09-26 13:19:48 +0530

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js app!');
+    res.send('This is from QA branch');
 });
 
 // Use Azure dynamic port
